b6128ec831e9bfad12c250458838ca78c0d6efd6
SimonQuerrioux <sim.querrioux@gmail.com>
2024-02-07 18:03:17 +0100

    HahA

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,55 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+
 export default function App() {
-  return <div>Odpri `src/App.jsx` in prični s pisanjem svoje aplikacije!</div>;
+  return (
+    <>
+      <div className="container">
+        <Button variant="outline">Button</Button>
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="avte">Avte</TabsTrigger>
+            <TabsTrigger value="ladje">Ladje</TabsTrigger>
+          </TabsList>
+          <TabsContent value="avte">
+            <Card>
+              <CardHeader>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="ladje">
+            <Card>
+              <CardHeader>
+                <CardTitle>Mercedes</CardTitle>
+                <CardDescription>Mercedes je </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 }
